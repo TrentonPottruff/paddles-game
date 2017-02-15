e33e798951bd55f0553f680524d80221d1d21d2a
Trenton Pottruff <jayitinc@gmail.com>
2017-02-15 11:26:45 -0500

    "Improved" bullet bouncing

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class Bullet : MonoBehaviour {
     public Vector2 direction;
+    public float speed;
 
     private Rigidbody2D rb;
 
     private void Start() {
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = direction * 2;
+        rb.velocity = direction.normalized * speed;
     }
 
     private void OnCollisionEnter2D(Collision2D col) {
@@ -26,8 +27,11 @@ public class Bullet : MonoBehaviour {
             multiplier = -1f;
 
         Vector2 newVector = Quaternion.Euler(0, 0, (angle * 2) * multiplier) * ballVector;
+        if (col.gameObject.tag.Equals("Paddle"))
+            newVector = Quaternion.Euler(0, 0, (-Vector2.Angle(col.transform.position, col.contacts[0].point)) * multiplier) * ballVector;
+
         Debug.DrawLine(col.contacts[0].point, (col.contacts[0].point + newVector), Color.red, 100000);
         direction = newVector.normalized;
-        rb.velocity = direction * 2;
+        rb.velocity = direction * speed;
     }
 }
--- Assets/SpawnPoint.cs
+++ Assets/SpawnPoint.cs
@@ -8,6 +8,8 @@ public class SpawnPoint : MonoBehaviour {
     public void SpawnBullet()
     {
         GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity) as GameObject;
+        Bullet script = bullet.GetComponent<Bullet>();
+        script.direction = Quaternion.Euler(0, 0, Random.Range(0, 360)) * Vector2.right;
     }
 
     private void Start()
