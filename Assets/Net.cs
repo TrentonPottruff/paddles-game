f89746bfaa961701796c7ffce43470147c0a9f36
TrentonPottruff2 <jayitinc@gmail.com>
2017-02-15 09:02:19 -0500

    Bullet Resets on Score

--- Assets/Net.cs
+++ Assets/Net.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Net : MonoBehaviour {
+    public SpawnPoint spawnPoint;
     public Score score;
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag.Equals("Bullet")) {
             score.score++;
+            Destroy(other.gameObject);
+            spawnPoint.SpawnBullet();
         }
     }
 }
---
+++ Assets/SpawnPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+    public GameObject bulletPrefab;
+
+    public void SpawnBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity) as GameObject;
+    }
+
+    private void Start()
+    {
+        SpawnBullet();
+    }
+}
