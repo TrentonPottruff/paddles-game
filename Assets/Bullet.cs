32794ffb7aac4ed0af42e02117c2351c48468ef6
Trenton Pottruff <jayitinc@gmail.com>
2017-02-14 23:14:33 -0500

    Paddle Bounces Slighty More Random

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -14,6 +14,14 @@ public class Bullet : MonoBehaviour {
     }
 
     private void OnCollisionEnter2D(Collision2D col) {
+        float additionalAngle = 0f;
+        if (col.gameObject.tag.Equals("Paddle")) {
+            Vector2 otherPos = col.contacts[0].otherCollider.transform.position;
+            float distance = Vector2.Distance(col.contacts[0].point, otherPos) * 20f;
+            Debug.DrawLine(col.contacts[0].point, otherPos, Color.magenta, 100000);
+            additionalAngle = 20f * distance;
+        }
+
         Vector2 ballVector = direction * -1f;
         Vector2 normalVector = col.contacts[0].normal;
         Debug.DrawLine(col.contacts[0].point, (col.contacts[0].point + ballVector), Color.blue, 100000);
@@ -24,7 +32,7 @@ public class Bullet : MonoBehaviour {
         if (cross.z < 0)
             multiplier = -1f;
 
-        Vector2 newVector = Quaternion.Euler(0, 0, (angle * 2) * multiplier) * ballVector;
+        Vector2 newVector = Quaternion.Euler(0, 0, (angle * 2 + additionalAngle) * multiplier) * ballVector;
         Debug.DrawLine(col.contacts[0].point, (col.contacts[0].point + newVector), Color.red, 100000);
         direction = newVector.normalized;
         rb.velocity = direction * 2;
